31901f62d7d0917bd357407762686cef9333248b
unknown <lsr488@gmail.com>
2022-03-13 14:52:22 -0400

    Bring in spinner.gif on load

--- src/components/layout/Spinner.jsx
+++ src/components/layout/Spinner.jsx
@@ -1,9 +1,9 @@
-import Spinner from './assets/spinner.gif';
+import spinner from './assets/spinner.gif';
 
 function Spinner() {
 	return (
 		<div className='w-100 mt-20'>
-			<img className="text-center mx-auto" src="{spinner}" alt="Loading..." width={180}/>
+			<img className="text-center mx-auto" src={spinner} alt="Loading..." width={180}/>
 		</div>
 	)
 }
