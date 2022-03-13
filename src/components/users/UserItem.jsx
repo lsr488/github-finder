4afe22cd608b48ad8f3ba1a9d1aaeb9cc41d8083
unknown <lsr488@gmail.com>
2022-03-13 15:09:19 -0400

    Destructured user array

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,9 +1,15 @@
-function UserItem() {
+import PropTypes from 'prop-types';
+
+function UserItem({user: {login, avatar_url}}) {
 	return (
 		<div>
-			user
+			{login}
 		</div>
 	)
+}
+
+UserItem.propTypes = {
+	user: PropTypes.object.isRequired
 }
 
 export default UserItem;
