15c213b1ee23076a0bb531515649488cae4611d8
unknown <lsr488@gmail.com>
2022-03-13 14:09:46 -0400

    Fixed home navbar link

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar({title}) {
 
 				<div className="flex-1 px-2 mx-2">
 					<div className="flex justify-end">
-						<Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
+						<Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
 							Home
 						</Link>
 						<Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
