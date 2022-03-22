205e4ea60590ad2ae2dea97ff635f5010860e23b
unknown <lsr488@gmail.com>
2022-03-22 09:24:01 -0400

    Bring in icons, Spinner, loading, and destructure user array

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,15 +1,38 @@
+import {FaCodepen, FaStore, FaUserFriends, FaUsers} from 'react-icons/fa';
 import { useEffect, useContext } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import Spinner from '../components/layout/Spinner';
 import GithubContext from '../context/github/GithubContext';
 
 function User() {
-	const { getUser, user } = useContext(GithubContext);
+	const { getUser, user, loading } = useContext(GithubContext);
 
 	const params = useParams();
 
 	useEffect(() => {
 		getUser(params.login)
 	}, []);
+
+	const {
+		name,
+		type,
+		avatar_url,
+		location,
+		bio,
+		blog,
+		twitter_username,
+		login,
+		html_url,
+		followers,
+		following,
+		public_repos,
+		public_gists,
+		hireable
+	} = user;
+
+	if(loading) {
+		return <Spinner />
+	}
 
 	return <div className="text-white">{user.login}</div>
 }
