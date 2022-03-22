7c4fbb936bcf943a5b23c2371cfcaff6ebe71f29
unknown <lsr488@gmail.com>
2022-03-22 09:19:52 -0400

    Search for user and display login name

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,5 +1,17 @@
+import { useEffect, useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import GithubContext from '../context/github/GithubContext';
+
 function User() {
-	return <div>USER</div>
+	const { getUser, user } = useContext(GithubContext);
+
+	const params = useParams();
+
+	useEffect(() => {
+		getUser(params.login)
+	}, []);
+
+	return <div className="text-white">{user.login}</div>
 }
 
 export default User;
