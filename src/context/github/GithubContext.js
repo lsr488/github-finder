e601d7ae771fa661b72ed435ce568a09facda3a0
unknown <lsr488@gmail.com>
2022-03-27 18:35:39 -0400

    Return sorted user repos

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -64,7 +64,12 @@ export const GithubProvider = ({children}) => {
 	const getUserRepos = async (login) => {
 		setLoading();
 
-		const response = await fetch(`${GITHUB_URL}/users/{login}/repos`, {
+		const params = new URLSearchParams({
+			sort: 'created',
+			per_page: 10
+		});
+
+		const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
 			headers: {
 				Authorization: `token ${GITHUB_TOKEN}`
 			}
